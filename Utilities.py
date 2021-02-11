cf87093b906c1182c23ae399c5b3828d9cd0a26e
Frederik Lillebæk Hansen <frederik.l.hansen@hotmail.com>
2021-02-11 10:12:17 +0100

    exercise 4

--- Utilities.py
+++ Utilities.py
@@ -19,3 +19,17 @@ def dndlamb(Lambda, Ns, Lambs):
         index = np.argmin(np.abs(Lambs-L))
         res.append(grads[index])
     return res
+
+def get_group_delay(z, nLambda, Lambda, dndlamb):
+    return (z/299792458)*(nLambda-Lambda*dndlamb)
+
+def get_pulse_duration(t0,Lambda,dndlamb,z):
+    #TODO: Enheder virker ikke
+    phimm = (Lambda**3/(2*np.pi*299792458**2))*dndlamb*z
+    return (t0*np.sqrt(1+np.power((4*np.log(2)*phimm)/((t0)**2),2)))
+
+def get_pulse_durations(t0s,Lambda,dndlamb,z):
+    res = []
+    for t0 in t0s:
+        res.append(get_pulse_duration(t0,Lambda,dndlamb,z))
+    return res
--- Week1.py
+++ Week1.py
@@ -2,28 +2,36 @@ import numpy as np
 import Utilities
 import matplotlib.pyplot as plt
 
-## Opgave 1)
+def opgave1():
+    print(Utilities.get_group_delay(1*10**-2, 1.45332, 0.8, -0.0173))
+    print(Utilities.get_group_delay(1 * 10 ** -2, 1.45601, 0.67, -0.0252))
+    print(Utilities.get_group_delay(1 * 10 ** -2, 1.47012, 0.4, -0.1091))
 
-# a)
-
-## Opgave 2)
-
-# a)
-
-
-if __name__ == "__main__":
+def opgave2():
     import seaborn
     seaborn.set_style(style="darkgrid")
 
-    lambdas = np.linspace(0.2,1.5,10000)
-    Bs = np.array([0.6961663,0.4079426,0.8974794])
-    Cs = np.array([0.0684043,0.1162414,9.896161])
-    Ns = [np.sqrt(e+1) for e in Utilities.get_sellMeier_list(lambdas, Bs, Cs)]
-    seaborn.lineplot(lambdas,Ns)
+    lambdas = np.linspace(0.2, 1.5, 10000)
+    Bs = np.array([0.6961663, 0.4079426, 0.8974794])
+    Cs = np.array([0.0684043, 0.1162414, 9.896161])
+    Ns = [np.sqrt(e + 1) for e in Utilities.get_sellMeier_list(lambdas, Bs, Cs)]
+    seaborn.lineplot(lambdas, Ns)
     plt.xlabel("Lambda")
     plt.ylabel("n")
     plt.show()
-    print(Utilities.dndlamb([0.8], Ns, lambdas))
+    print(Utilities.dndlamb([0.4, 0.67, 0.8], Ns, lambdas))
+
+def opgave4():
+    tstart = [10, 20, 100, 1000]
+    lambA = 800*10**-9
+    lambB = 600*10**-9
+    print(Utilities.get_pulse_durations(tstart,lambA, 0.49*10**-11, 0.01))
+    print(Utilities.get_pulse_durations(tstart, lambB, 1.79 * 10 ** -11, 0.01))
+
+if __name__ == "__main__":
+    # opgave1()
+    # opgave2()
+    opgave4()
 
 
 
